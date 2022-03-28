c67e6b03d91a269ce4b73dfe7a16b094881dd150
Tobias Jeppesen <github@tobiasjeppesen.com>
2022-03-28 13:49:51 +0200

    Fix

--- fix-dock-items.sh
+++ fix-dock-items.sh
@@ -6,7 +6,6 @@
 ###############################################################################
 
 
-dockutil=$(which dockutil)
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
 homeDict="/Users/zensum/"
 if [ "$currentUser" != "loginwindow" ]; then
@@ -20,40 +19,41 @@ if [ "$currentUser" != "loginwindow" ]; then
   fi
 
 
-"$dockutil" –-remove all –no-restart
+dockutil –-remove all –no-restart
 sleep 1
 
-if [ -d "/Applications/Slack.app" ] ; then
-	"$dockutil" --add "/Applications/Slack.app" --no-restart "$homeDict"
-else
-	echo "Slack not installed, skipping Dock placement"
-fi
-
-if [ -d "/Applications/Telavox.app" ] ; then
-	"$dockutil" --add "/Applications/Telavox.app" --position 1 --no-restart "$homeDict"
-else
-	echo "Telavox not installed, skipping Dock placement"
-fi
-
-
-if [ -d "/Applications/Filemaker.app" ] ; then
-	"$dockutil" --add "/Applications/Filemaker.app" --position 1 --no-restart "$homeDict"
-else
-	echo "Filemaker not installed, skipping Dock placement"
-fi
+dockutil --add "/Applications/Safari.app" --position 1 --no-restart "$homeDict"
 
 if [ -d "/Applications/Google Chrome.app" ] ; then
-	"$dockutil" --add "/Applications/Google Chrome.app" --position 1 --no-restart "$homeDict"
+	dockutil --add "/Applications/Google Chrome.app" --after Safari --no-restart "$homeDict"
 else
 	echo "Google Chrome not installed, skipping Dock placement"
 fi
 
-if [ -d "/Applications/Safari.app" ] ; then
-	"$dockutil" --add "/Applications/Safari.app" --position 1 --no-restart "$homeDict"
+if [ -d "/Applications/Telavox.app" ] ; then
+	dockutil --add "/Applications/Telavox.app" --after "Google Chrome" --no-restart "$homeDict"
 else
-	echo "Safari not installed, skipping Dock placement"
+	echo "Telavox not installed, skipping Dock placement"
 fi
 
-"$dockutil" --add "/Applications/Zensum App Store.app" --restart --position 100 "$homeDict"
+if [ -d "/Applications/Slack.app" ] ; then
+	dockutil --add "/Applications/Slack.app" --after Telavox --no-restart "$homeDict"
+else
+	echo "Slack not installed, skipping Dock placement"
+fi
+
+dockutil --add "/Applications/Calculator.app" --after Slack --no-restart "$homeDict"
+
+dockutil --add "/Applications/Notes.app" --after Calculator --no-restart "$homeDict"
+
+dockutil --add "/Applications/Calendar.app" --after Notes --no-restart "$homeDict"
+
+if [ -d "/Applications/Filemaker.app" ] ; then
+	dockutil --add "/Applications/Filemaker.app" --after Slack --no-restart "$homeDict"
+else
+	echo "Filemaker not installed, skipping Dock placement"
+fi
+
+dockutil --add "/Applications/Zensum App Store.app" --restart --position 100 "$homeDict"
 
 exit 0
