6ca416133e9e723a26dc885c6905522456063a6e
Tobias Jeppesen <github@tobiasjeppesen.com>
2022-03-28 13:19:39 +0200

    Fix

--- fix-dock-items.sh
+++ fix-dock-items.sh
@@ -8,23 +8,6 @@
 
 dockutil=$(which dockutil)
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-# uid=$(id -u "$currentUser")
-
-# convenience function to run a command as the current user
-# usage:
-#   runAsUser command arguments...
-# runAsUser() {
-#   if [ "$currentUser" != "loginwindow" ]; then
-#     # launchctl asuser "$uid" sudo -u "$currentUser" "$@"
-#     su "$currentUser" -c "$@"
-#   else
-#     echo "no user logged in"
-#     # uncomment the exit command
-#     # to make the function exit with an error when no user is logged in
-#     exit 1
-#   fi
-# }
-
 if [ "$currentUser" != "loginwindow" ]; then
     # launchctl asuser "$uid" sudo -u "$currentUser" "$@"
     echo "Running as $currentUser"
@@ -35,27 +18,29 @@ if [ "$currentUser" != "loginwindow" ]; then
     exit 1
   fi
 
+plistFile="/Users/zensum/Library/Preferences/com.apple.dock.plist "
+
 
 ###############################################################################
 ############################### APPLE DEFAULTS ################################
 ###############################################################################
 
 
-su "$currentUser" -c "$dockutil" --remove 'Launchpad' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Messages' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Launchpad' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Maps' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Photos' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'FaceTime' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Contacts' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Reminders' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Notes' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'TV' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Music' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Podcasts' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'News' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Numbers' --no-restart
-su "$currentUser" -c "$dockutil" --remove 'Pages' --no-restart
+"$dockutil" --remove 'Launchpad' --no-restart "$plistFile"
+"$dockutil" --remove 'Messages' --no-restart "$plistFile"
+"$dockutil" --remove 'Launchpad' --no-restart "$plistFile"
+"$dockutil" --remove 'Maps' --no-restart "$plistFile"
+"$dockutil" --remove 'Photos' --no-restart "$plistFile"
+"$dockutil" --remove 'FaceTime' --no-restart "$plistFile"
+"$dockutil" --remove 'Contacts' --no-restart "$plistFile"
+"$dockutil" --remove 'Reminders' --no-restart "$plistFile"
+"$dockutil" --remove 'Notes' --no-restart "$plistFile"
+"$dockutil" --remove 'TV' --no-restart "$plistFile"
+"$dockutil" --remove 'Music' --no-restart "$plistFile"
+"$dockutil" --remove 'Podcasts' --no-restart "$plistFile"
+"$dockutil" --remove 'News' --no-restart "$plistFile"
+"$dockutil" --remove 'Numbers' --no-restart "$plistFile"
+"$dockutil" --remove 'Pages' --no-restart "$plistFile"
 
 
 ###############################################################################
@@ -64,37 +49,37 @@ su "$currentUser" -c "$dockutil" --remove 'Pages' --no-restart
 
 
 if [ -d "/Applications/Slack.app" ] ; then
-	su "$currentUser" -c "$dockutil" --add "/Applications/Slack.app" --no-restart
+	"$dockutil" --add "/Applications/Slack.app" --no-restart "$plistFile"
 else
 	echo "Slack not installed, skipping Dock placement"
 fi
 
 if [ -d "/Applications/Telavox.app" ] ; then
-	su "$currentUser" -c "$dockutil" --add "/Applications/Telavox.app" --position 1 --no-restart
+	"$dockutil" --add "/Applications/Telavox.app" --position 1 --no-restart "$plistFile"
 else
 	echo "Telavox not installed, skipping Dock placement"
 fi
 
 
 if [ -d "/Applications/Filemaker.app" ] ; then
-	su "$currentUser" -c "$dockutil" --add "/Applications/Filemaker.app" --position 1 --no-restart
+	"$dockutil" --add "/Applications/Filemaker.app" --position 1 --no-restart "$plistFile"
 else
 	echo "Filemaker not installed, skipping Dock placement"
 fi
 
 if [ -d "/Applications/Google Chrome.app" ] ; then
-	su "$currentUser" -c "$dockutil" --add "/Applications/Google Chrome.app" --position 1 --no-restart
+	"$dockutil" --add "/Applications/Google Chrome.app" --position 1 --no-restart "$plistFile"
 else
 	echo "Google Chrome not installed, skipping Dock placement"
 fi
 
 if [ -d "/Applications/Safari.app" ] ; then
-	su "$currentUser" -c "$dockutil" --add "/Applications/Safari.app" --position 1 --no-restart
+	"$dockutil" --add "/Applications/Safari.app" --position 1 --no-restart "$plistFile"
 else
 	echo "Safari not installed, skipping Dock placement"
 fi
 
-# su "$currentUser" -c "$dockutil" --add "/Applications/Zensum App Store.app" --before "Safari" --no-restart
-su "$currentUser" -c "$dockutil" --add "/Applications/Zensum App Store.app" --restart
+# "$dockutil" --add "/Applications/Zensum App Store.app" --before "Safari" --no-restart "$plistFile"
+"$dockutil" --add "/Applications/Zensum App Store.app" --restart --postition 100 "$plistFile"
 
 exit 0
