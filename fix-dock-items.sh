b55a174b768946450133158fdca12ee252fd8a99
Tobias Jeppesen <github@tobiasjeppesen.com>
2022-04-01 14:16:01 +0200

    Fix

--- fix-dock-items.sh
+++ fix-dock-items.sh
@@ -7,7 +7,7 @@
 
 
 currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-homeDict="/Users/zensum/"
+homeDict="/Users/$currentUser/"
 if [ "$currentUser" != "loginwindow" ]; then
     # launchctl asuser "$uid" sudo -u "$currentUser" "$@"
     echo "Running as $currentUser"
@@ -19,8 +19,23 @@ if [ "$currentUser" != "loginwindow" ]; then
   fi
 
 
-dockutil –-remove all –-no-restart --allhomes
+dockutil --remove all --no-restart $homeDict
+# dock_items_str="$(dockutil --list $homeDict | awk -F '\t' '{print $1}')"
+# IFS='
+# '
+# #Read the split lines an array based on comma delimiter
+# read -a strarr <<< "$dock_items_Str"
+# for item in $dock_items;
+# do
+#   echo "Removing $item"
+#   dockutil_command="dockutil –-remove '$item' $homeDict --no-restart"
+#   echo "Running command: $dockutil_command"
+#   /bin/bash -c "$($dockutil_command)"
+#   sleep 15
+# done
 sleep 1
+
+dockutil --list $homeDict | awk -F "	" '{print $1}'
 
 dockutil --add "/Applications/Safari.app" --position 1 --no-restart --allhomes
 
---
+++ install-filemaker.md
@@ -0,0 +1,7 @@
+# Install filemaker
+
+Source: https://community.jamf.com/t5/jamf-pro/managed-installation-of-filemaker-pro-16/m-p/204924#M193625
+
+1. Download Filemaker Pro dmg
+2. Head to filemaker.com and download https://www.claris.com/resources/documentation/docs/fmp_osx_deployment_fmp1516.zip
+3. Run the above script with the folder as argument the Filemaker files, FileMaker Pro 16.pkg, Assisted Install.txt, Registration.app and Extras as argument
